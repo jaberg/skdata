1c952a93d9885fa252d702948c9fc063964ce248
James Bergstra <james.bergstra@gmail.com>
2011-09-02 11:52:06 -0400

    adding tasks.py

---
+++ datasets/tasks.py
@@ -0,0 +1,22 @@
+"""Task API
+"""
+
+def assert_classification(X, y, N=None):
+    A, B = X.shape
+    C, = y.shape
+    assert A == C == (C if N is None else N)
+    assert 'float' in str(X.dtype)
+    assert 'int' in str(y.dtype), y.dtype
+
+
+def assert_regression(X, Y, N=None):
+    A, B = X.shape
+    C, D = Y.shape
+    assert A == C == (C if N is None else N)
+    assert 'float' in str(X.dtype)
+    assert 'float' in str(Y.dtype)
+
+def assert_clustering(X, N=None):
+    A, B = X.shape
+    assert A == (A if N is None else N)
+    assert 'float' in str(X.dtype)
