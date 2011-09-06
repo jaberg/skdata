a57199140b173e102db69f7042140b9af8c532b7
Nicolas Pinto <pinto@mit.edu>
2011-09-06 01:35:23 -0400

    Update pubfig83

--- datasets/pubfig83.py
+++ datasets/pubfig83.py
@@ -1,97 +1,173 @@
-"""
-XXX Description of dataset (Zak?)
+# -*- coding: utf-8 -*-
+"""PubFig83 Dataset
 
-XXX Citation
+http://www.eecs.harvard.edu/~zak/pubfig83
 
+If you make use of this data, please cite the following paper:
+
+"Scaling-up Biologically-Inspired Computer Vision: A Case-Study on Facebook."
+Nicolas Pinto, Zak Stone, Todd Zickler, David D. Cox
+Proc. CVPR Workshop on Biologically Consistent Vision (2011).
+
+Please consult the publication for further information.
 """
+
+# Copyright (C) 2011
+# Authors: Zak Stone <zak@eecs.harvard.edu>
+#          Dan Yamins <yamins@mit.edu>
+#          James Bergstra <bergstra@rowland.harvard.edu>
+#          Nicolas Pinto <pinto@rowland.harvard.edu>
+# License: Simplified BSD
+
+# XXX: splits (csv-based) for verification and identification tasks (CVPR'11)
+
+
 import os
-import sys
+from os import path
+import shutil
+from glob import glob
+import hashlib
 
 from data_home import get_data_home
-import utils, utils.image
+from utils import download, extract
+import utils
+import utils.image
 
-_genders = ['M', 'M', 'F', 'F', 'M', 'F', 'M', 'M', 'F', 'M', 'F', 'F', 'F',
-'F', 'F', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'F', 'F', 'M', 'M', 'F', 'F',
-'M', 'M', 'F', 'F', 'M', 'M', 'M', 'M', 'F', 'F', 'F', 'F', 'F', 'M', 'M', 'F',
-'F', 'F', 'F', 'F', 'F', 'M', 'M', 'F', 'F', 'F', 'M', 'F', 'F', 'M', 'M', 'F',
-'M', 'F', 'F', 'M', 'F', 'F', 'M', 'M', 'F', 'F', 'M', 'F', 'F', 'M', 'M', 'M',
-'M', 'F', 'F', 'M', 'M', 'M']
 
 class PubFig83(object):
-    """
-    XXX Technical description of class (attributes, methods, etc.) rather than
-    descr of dataset (which is above in the file).
+    """PubFig83 Face Dataset
 
-    self.meta a list of dictionaries. Each one contains
-        gender: 'M' or 'F'
-        name: str
-        id: int
-        jpgfile: relpath
+    Attributes
+    ----------
+    meta: list of dict
+        Metadata associated with the dataset. For each image with index i,
+        meta[i] is a dict with keys:
+            name: str
+                Name of the individual's face in the image.
+            filename: str
+                Full path to the image.
+            gender: str
+                'male or 'female'
+            id: int
+                Identifier of the image.
+            sha1: str
+                SHA-1 hash of the image.
+
+    Notes
+    -----
+    If joblib is available, then `meta` will be cached for faster
+    processing. To install joblib use 'pip install -U joblib' or
+    'easy_install -U joblib'.
     """
+
+    URL = 'http://www.eecs.harvard.edu/~zak/pubfig83/pubfig83_first_draft.tgz'
+    SHA1 = '1fd55188bf7d9c5cc9d68baee57aa09c41bd2246'
+
+    _GENDERS = ['male', 'male', 'female', 'female', 'male', 'female', 'male',
+                'male', 'female', 'male', 'female', 'female', 'female',
+                'female', 'female', 'male', 'male', 'male', 'male', 'male',
+                'male', 'male', 'male', 'female', 'female', 'male', 'male',
+                'female', 'female', 'male', 'male', 'female', 'female', 'male',
+                'male', 'male', 'male', 'female', 'female', 'female', 'female',
+                'female', 'male', 'male', 'female', 'female', 'female',
+                'female', 'female', 'female', 'male', 'male', 'female',
+                'female', 'female', 'male', 'female', 'female', 'male', 'male',
+                'female', 'male', 'female', 'female', 'male', 'female',
+                'female', 'male', 'male', 'female', 'female', 'male', 'female',
+                'female', 'male', 'male', 'male', 'male', 'female', 'female',
+                'male', 'male', 'male']
 
     def __init__(self, meta=None):
         if meta is not None:
             self._meta = meta
-    #
-    # Standard dataset object interface
-    #
 
-    def __get_meta(self):
+        self.name = self.__class__.__name__
+
         try:
-            return self._meta
-        except AttributeError:
-            self.fetch(download_if_missing=True)
-            self._meta = self.build_meta()
-            return self._meta
-    meta = property(__get_meta)
+            from joblib import Memory
+            mem = Memory(cachedir=self.home('cache'))
+            self._get_meta = mem.cache(self._get_meta)
+        except ImportError:
+            pass
 
-    #
-    # Helper routines
-    #
+    def home(self, *suffix_paths):
+        return path.join(get_data_home(), self.name, *suffix_paths)
 
-    def home(self, *names):
-        return os.path.join(get_data_home(), 'pubfig83', *names)
-
-    def build_meta(self):
-        names = sorted(os.listdir(self.home('pubfig83')))
-        assert len(names) == len(_genders)
-        meta = []
-        ind = 0
-        for gender, name in zip(_genders, names):
-            for pic in sorted(os.listdir(self.home('pubfig83', name))):
-                meta.append(dict(
-                    gender=gender,
-                    name=name,
-                    id=ind,
-                    jpgfile=pic))
-                ind +=1
-        return meta
+    # ------------------------------------------------------------------------
+    # -- Dataset Interface: fetch()
+    # ------------------------------------------------------------------------
 
     def fetch(self, download_if_missing=True):
         """Download and extract the dataset."""
-        dataset_dir = self.home()
-        if os.path.exists(dataset_dir):
-            return
+
+        home = self.home()
+
         if not download_if_missing:
-            raise IOError(dataset_dir)
-        os.makedirs(dataset_dir)
-        # XXX: use urllib instead of wget
-        os.system('wget http://www.eecs.harvard.edu/~zak/pubfig83/pubfig83_first_draft.tgz -P ' + dataset_dir)
-        # XXX: use python's tar wrapper instead of this command which
-        #      will (I think) not work on windows
-        os.system('cd ' + dataset_dir + '; tar -xzf pubfig83_first_draft.tgz')
+            raise IOError("'%s' exists!" % home)
+
+        # download archive
+        url = self.URL
+        sha1 = self.SHA1
+        basename = path.basename(url)
+        archive_filename = path.join(home, basename)
+        if not path.exists(archive_filename):
+            if not download_if_missing:
+                return
+            if not path.exists(home):
+                os.makedirs(home)
+            download(url, archive_filename, sha1=sha1)
+
+        # extract it
+        if not path.exists(self.home('pubfig83')):
+            extract(archive_filename, home, sha1=sha1, verbose=True)
+
+    # ------------------------------------------------------------------------
+    # -- Dataset Interface: meta
+    # ------------------------------------------------------------------------
+
+    @property
+    def meta(self):
+        if hasattr(self, '_meta'):
+            return self._meta
+        else:
+            self.fetch(download_if_missing=True)
+            self._meta = self._get_meta()
+            return self._meta
+
+    def _get_meta(self):
+        names = sorted(os.listdir(self.home('pubfig83')))
+        genders = self._GENDERS
+        assert len(names) == len(genders)
+        meta = []
+        ind = 0
+        for gender, name in zip(genders, names):
+            img_filenames = sorted(glob(self.home('pubfig83', name, '*.jpg')))
+            for img_filename in img_filenames:
+                img_data = open(img_filename, 'rb').read()
+                sha1 = hashlib.sha1(img_data).hexdigest()
+                meta.append(dict(gender=gender, name=name, id=ind,
+                                 filename=img_filename, sha1=sha1))
+                ind += 1
+        return meta
+
+    # ------------------------------------------------------------------------
+    # -- Dataset Interface: clean_up()
+    # ------------------------------------------------------------------------
 
     def clean_up(self):
-        if isdir(self.home()):
+        if path.isdir(self.home()):
             shutil.rmtree(self.home())
+
+    # ------------------------------------------------------------------------
+    # -- Helpers
+    # ------------------------------------------------------------------------
 
     def image_path(self, m):
         return self.home('pubfig83', m['name'], m['jpgfile'])
 
-    #
-    # Standard Tasks
-    # --------------
-    #
+    # ------------------------------------------------------------------------
+    # -- Standard Tasks
+    # ------------------------------------------------------------------------
 
     def raw_recognition_task(self):
         names = [m['name'] for m in self.meta]
@@ -105,17 +181,18 @@ class PubFig83(object):
         return paths, utils.int_labels(genders)
 
 
-#
-# Drivers for scikits.data/bin executables
-#
+# ------------------------------------------------------------------------
+# -- Drivers for scikits.data/bin executables
+# ------------------------------------------------------------------------
 
 def main_fetch():
     """compatibility with bin/datasets-fetch"""
-    fetch()
+    PubFig83.fetch(download_if_missing=True)
+
 
 def main_show():
     """compatibility with bin/datasets-show"""
-    from utils.glviewer import glumpy_viewer, command, glumpy
+    from utils.glviewer import glumpy_viewer
     import larray
     pf = PubFig83()
     names = [m['name'] for m in pf.meta]
@@ -123,5 +200,3 @@ def main_show():
     glumpy_viewer(
             img_array=larray.lmap(utils.image.ImgLoader(), paths),
             arrays_to_print=[names])
-
-
