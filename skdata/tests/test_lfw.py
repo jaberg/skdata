aafa003657c006239e353fe457177d2adc7fcad3
Dan Yamins <dyamins@gmail.com>
2011-11-05 23:25:40 -0400

    more tests of lfw respltting

--- skdata/tests/test_lfw.py
+++ skdata/tests/test_lfw.py
@@ -235,9 +235,13 @@ def test_img_verification_task():
 
 def test_img_verification_resplit():
     dset = lfw.Original()
-    X, Y, z = dset.img_verification_task(resplit='train_1')
+    X, Y, z = dset.raw_verification_task_resplit('train_1')
     assert len(X) == 2200
     assert (z[:10] == np.array([0, 1, 1, 0, 1, 1, 0, 0, 0, 0])).all()
-    X, Y, z = dset.img_verification_task(resplit='test_3')
-    assert len(X) == 1000
+    X1, Y1, z1 = dset.raw_verification_task_resplit('test_1')
+    assert len(X1) == 1000
+    assert set(zip(X, Y)).intersection(zip(X1, Y1)) == set([])
+    X, Y, z = dset.raw_verification_task_resplit('test_3')
     assert (z[:10] == np.array([1, 1, 1, 1, 0, 0, 0, 0, 0, 1])).all()
+    X, Y, z = dset.raw_verification_task_resplit('train_1', seed=1)
+    assert (z[:10] == np.array([0, 0, 1, 1, 1, 0, 0, 1, 1, 0])).all()
