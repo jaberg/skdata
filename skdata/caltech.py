a763540ac0aa161056043375ee4f8dfc92437e76
James Bergstra <james.bergstra@gmail.com>
2012-04-27 16:17:26 -0400

    caltech: refactor take 1

--- skdata/caltech.py
+++ skdata/caltech.py
@@ -63,17 +63,10 @@ class BaseCaltech(object):
     'easy_install -U joblib'.
     """
 
-    def __init__(self, meta=None, seed=0, ntrain=15, ntest=15, num_splits=10):
-
-        self.seed = seed
-        self.ntrain = ntrain
-        self.ntest = ntest
-        self.num_splits = num_splits
+    def __init__(self, meta=None ):
 
         if meta is not None:
             self._meta = meta
-
-        self.name = self.__class__.__name__
 
         try:
             from joblib import Memory
@@ -83,7 +76,8 @@ class BaseCaltech(object):
             pass
 
     def home(self, *suffix_paths):
-        return path.join(get_data_home(), self.name, *suffix_paths)
+        subdir = self.__class__.__name__
+        return path.join(get_data_home(), subdir, *suffix_paths)
 
     # ------------------------------------------------------------------------
     # -- Dataset Interface: fetch()
@@ -122,68 +116,72 @@ class BaseCaltech(object):
         if not hasattr(self, '_meta'):
             self.fetch(download_if_missing=True)
             self._meta = self._get_meta()
-            self.names = sorted(os.listdir(self.home(self.SUBDIR)))
         return self._meta
 
     def _get_meta(self):
-        try:
-            rval = cPickle.load(
-                    open(
-                        self.home(self.SUBDIR + '.meta.pkl')))
-            open(rval[0]['filename'])
-            return rval
-        except IOError:
-            # IOError may come either from a missing pkl file
-            # or from a missing image (rval[0]['filename']) but in both
-            # cases the response is to rebuild the metadata
-            names = sorted(os.listdir(self.home(self.SUBDIR)))
+        names = sorted(os.listdir(self.home(self.SUBDIR)))
 
-            meta = []
-            ind = 0
+        meta = []
+        ind = 0
 
-            for name in names:
+        for name in names:
 
-                pattern = self.home(self.SUBDIR, name, '*.jpg')
+            pattern = self.home(self.SUBDIR, name, '*.jpg')
 
-                img_filenames = sorted(glob(pattern))
+            img_filenames = sorted(glob(pattern))
 
-                for img_filename in img_filenames:
-                    img_data = open(img_filename, 'rb').read()
-                    sha1 = hashlib.sha1(img_data).hexdigest()
+            for img_filename in img_filenames:
+                img_data = open(img_filename, 'rb').read()
+                sha1 = hashlib.sha1(img_data).hexdigest()
 
-                    data = dict(name=name,
-                                id=ind,
-                                filename=img_filename,
-                                sha1=sha1)
+                data = dict(name=name,
+                            id=ind,
+                            filename=img_filename,
+                            sha1=sha1)
 
-                    meta.append(data)
-                    ind += 1
+                meta.append(data)
+                ind += 1
 
-            cPickle.dump(
-                    meta,
-                    open(self.home(self.SUBDIR + '.meta.pkl'), 'w'))
+        return meta
 
-            return meta
 
-    @property
-    def splits(self):
+    # ------------------------------------------------------------------------
+    # -- Dataset Interface: clean_up()
+    # ------------------------------------------------------------------------
+
+    def clean_up(self):
+        if path.isdir(self.home()):
+            shutil.rmtree(self.home())
+
+
+class Caltech101(BaseCaltech):
+    URL = ('http://www.vision.caltech.edu/Image_Datasets/'
+           'Caltech101/101_ObjectCategories.tar.gz')
+    SHA1 = 'b8ca4fe15bcd0921dfda882bd6052807e63b4c96'
+    SUBDIR = '101_ObjectCategories'
+
+
+class Caltech256(BaseCaltech):
+    URL = ('http://www.vision.caltech.edu/Image_Datasets/'
+           'Caltech256/256_ObjectCategories.tar')
+    SHA1 = '2195e9a478cf78bd23a1fe51f4dabe1c33744a1c'
+    SUBDIR = '256_ObjectCategories'
+
+
+class KfoldClassification(object):
+    def __init__(self, caltech, K, n_train, n_test, seed):
         """
-        generates splits and attaches them in the "splits" attribute
+        Constructs splits for K-fold cross-validation of a classifier.
+
+        caltech - a low-level caltech object
+        K - number of splits
+        n_train - int
+        n_test - int
+        seed - int
 
         """
-        if not hasattr(self, '_splits'):
-            seed = self.seed
-            ntrain = self.ntrain
-            ntest = self.ntest
-            num_splits = self.num_splits
-            self._splits = self.generate_splits(seed, ntrain,
-                                                ntest, num_splits)
-        return self._splits
-
-    def generate_splits(self, seed, ntrain, ntest, num_splits):
-        meta = self.meta
-        ntrain = self.ntrain
-        ntest = self.ntest
+        self.caltech = caltech
+        meta = caltech.meta
         rng = np.random.RandomState(seed)
         splits = {}
         for split_id in range(num_splits):
@@ -198,15 +196,14 @@ class BaseCaltech(object):
                     splits['train_' + str(split_id)].append(cat[ind]['id'])
                 for ind in perm[ntrain: ntrain + ntest]:
                     splits['test_' + str(split_id)].append(cat[ind]['id'])
-        return splits
+        self._splits = splits
 
-    # ------------------------------------------------------------------------
-    # -- Dataset Interface: clean_up()
-    # ------------------------------------------------------------------------
+    def __len__(self):
+        return len(self._splits)
 
-    def clean_up(self):
-        if path.isdir(self.home()):
-            shutil.rmtree(self.home())
+    def __getitem__(self, k):
+        # return (train_x, train_y), (test_x, test_y) ?
+        raise NotImplementedError()
 
     # ------------------------------------------------------------------------
     # -- Standard Tasks
@@ -228,17 +225,3 @@ class BaseCaltech(object):
         imgs = larray.lmap(ImgLoader(ndim=3, dtype=dtype, mode='RGB'),
                            img_paths)
         return imgs, labels
-
-
-class Caltech101(BaseCaltech):
-    URL = ('http://www.vision.caltech.edu/Image_Datasets/'
-           'Caltech101/101_ObjectCategories.tar.gz')
-    SHA1 = 'b8ca4fe15bcd0921dfda882bd6052807e63b4c96'
-    SUBDIR = '101_ObjectCategories'
-
-
-class Caltech256(BaseCaltech):
-    URL = ('http://www.vision.caltech.edu/Image_Datasets/'
-           'Caltech256/256_ObjectCategories.tar')
-    SHA1 = '2195e9a478cf78bd23a1fe51f4dabe1c33744a1c'
-    SUBDIR = '256_ObjectCategories'
