dcc3cc2fd3573e75a2dd83f709adbd50fe6d5db2
James Bergstra <james.bergstra@gmail.com>
2013-07-06 17:10:54 -0400

    adding more views for Larochelle etal 2007

--- skdata/larochelle_etal_2007/view.py
+++ skdata/larochelle_etal_2007/view.py
@@ -1,10 +1,7 @@
 import dataset
 from ..base import Task
 
-class RectanglesVectorXV(object):
-    def __init__(self):
-        self.dataset = dataset.Rectangles()
-
+class VectorXV(object):
     def protocol(self, algo):
         ds = self.dataset
         ds.fetch(True)
@@ -19,7 +16,7 @@ class RectanglesVectorXV(object):
                 name='train',
                 x=ds._inputs[start:end].reshape((end-start), -1),
                 y=ds._labels[start:end],
-                n_classes=2)
+                n_classes=ds.descr['n_classes'])
 
         start = n_train
         end = n_train + n_valid
@@ -27,7 +24,7 @@ class RectanglesVectorXV(object):
                 name='valid',
                 x=ds._inputs[start:end].reshape((end-start), -1),
                 y=ds._labels[start:end],
-                n_classes=2)
+                n_classes=ds.descr['n_classes'])
 
         start = n_train + n_valid
         end = n_train + n_valid + n_test
@@ -35,11 +32,80 @@ class RectanglesVectorXV(object):
                 name='test',
                 x=ds._inputs[start:end].reshape((end-start), -1),
                 y=ds._labels[start:end],
-                n_classes=2)
+                n_classes=ds.descr['n_classes'])
 
         model = algo.best_model(train=train, valid=valid)
         algo.loss(model, train)
         algo.loss(model, valid)
         return algo.loss(model, test)
 
+
+class MNIST_Basic_VectorXV(VectorXV):
+    def __init__(self):
+        self.dataset = dataset.MNIST_Basic()
+
+
+class MNIST_BackgroundImages_VectorXV(VectorXV):
+    def __init__(self):
+        self.dataset = dataset.MNIST_BackgroundImages()
+
+
+class MNIST_BackgroundRandom_VectorXV(VectorXV):
+    def __init__(self):
+        self.dataset = dataset.MNIST_BackgroundRandom()
+
+
+class MNIST_Rotated_VectorXV(VectorXV):
+    def __init__(self):
+        self.dataset = dataset.MNIST_Rotated()
+
+
+class MNIST_RotatedBackgroundImages_VectorXV(VectorXV):
+    def __init__(self):
+        self.dataset = dataset.MNIST_RotatedBackgroundImages()
+
+
+class MNIST_Noise1_VectorXV(VectorXV):
+    def __init__(self):
+        self.dataset = dataset.MNIST_Noise1()
+
+
+class MNIST_Noise2_VectorXV(VectorXV):
+    def __init__(self):
+        self.dataset = dataset.MNIST_Noise2()
+
+
+class MNIST_Noise3_VectorXV(VectorXV):
+    def __init__(self):
+        self.dataset = dataset.MNIST_Noise3()
+
+
+class MNIST_Noise4_VectorXV(VectorXV):
+    def __init__(self):
+        self.dataset = dataset.MNIST_Noise4()
+
+
+class MNIST_Noise5_VectorXV(VectorXV):
+    def __init__(self):
+        self.dataset = dataset.MNIST_Noise5()
+
+
+class MNIST_Noise6_VectorXV(VectorXV):
+    def __init__(self):
+        self.dataset = dataset.MNIST_Noise6()
+
+
+class RectanglesVectorXV(VectorXV):
+    def __init__(self):
+        self.dataset = dataset.Rectangles()
+
+
+class RectanglesImagesVectorXV(VectorXV):
+    def __init__(self):
+        self.dataset = dataset.RectanglesImages()
+
+
+class ConvexVectorXV(VectorXV):
+    def __init__(self):
+        self.dataset = dataset.Convex()
 
