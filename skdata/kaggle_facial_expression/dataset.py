31be272f8426ca3458f37a29ba7ef784a17ae727
James Bergstra <james.bergstra@gmail.com>
2013-10-24 15:48:32 -0400

    skdata: data has moved since contest close

--- skdata/kaggle_facial_expression/dataset.py
+++ skdata/kaggle_facial_expression/dataset.py
@@ -37,18 +37,34 @@ contest.
 
 import cPickle
 import lockfile
+import logging
 import os
+import shutil
 
 import numpy as np
 
 from skdata.data_home import get_data_home
-from skdata.utils import download
-from skdata.utils.download_and_extract import verify_sha1
+from skdata.utils.download_and_extract import verify_sha1, extract
+
+logger = logging.getLogger(__name__)
 
 FILES_SHA1s = [
     ('test.csv', '3f9199ae6e9a40137e72cb63264490e622d9c798'),
     ('train.csv', '97651a1fffc7e0af22ebdd5de36700b0e7e0c12c'),
     ('example_submission.csv', '14fac7ef24e3ab6d9fcaa9edd273fe08abfa5c51')]
+
+TGZ_FILENAME = 'fer2013.tar.gz'
+
+FULL_URL = '/'.join([
+    'http://www.kaggle.com',
+    'c',
+    ('challenges-in-representation-learning'
+     '-facial-expression-recognition-challenge'),
+    'download',
+    TGZ_FILENAME])
+
+TGZ_SHA1 = 'b0e7632c70853f4d3b6a2a73031dd8c71d8d536d'
+
 
 class KaggleFacialExpression(object):
     N_TRAIN = 28709
@@ -64,14 +80,20 @@ class KaggleFacialExpression(object):
     # -- Dataset Interface: fetch()
     # ------------------------------------------------------------------------
 
-    def fetch(self, download_if_missing=True):
+    def install(self, local_fer2013):
         """
-        Verify data only. Data must be downloaded from kaggle.
+        Verify SHA1 and copy given file into .skdata cache directory.
         """
+        verify_sha1(local_fer2013, TGZ_SHA1)
+        if not os.path.isdir(self.home()):
+            os.makedirs(self.home())
+        lock = lockfile.FileLock(self.home())
+        if lock.is_locked():
+            logger.warn('%s is locked, waiting for release' % self.home())
 
-        for fname, sha1 in FILES_SHA1s:
-            local_filename = self.home(fname)
-            verify_sha1(local_filename, sha1)
+        with lock:
+            shutil.copyfile(local_fer2013, self.home(TGZ_FILENAME))
+            extract(self.home(TGZ_FILENAME), self.home())
 
     @property
     def meta(self):
@@ -84,7 +106,10 @@ class KaggleFacialExpression(object):
     def _get_meta(self):
         filename = 'meta_%s.pkl' % self._build_meta_version
         try:
-            meta = cPickle.load(open(self.home(filename)))
+            if self._build_meta_version:
+                meta = cPickle.load(open(self.home(filename)))
+            else:
+                raise IOError()
         except (IOError, cPickle.PickleError), e:
             meta = self._build_meta()
             outfile = open(self.home(filename), 'wb')
@@ -92,37 +117,26 @@ class KaggleFacialExpression(object):
             outfile.close()
         return meta
 
-    _build_meta_version = '2'
+    _build_meta_version = '3'
     def _build_meta(self):
         meta = []
 
         # -- load train.csv
-        for ii, line in enumerate(open(self.home('train.csv'))):
+        for ii, line in enumerate(open(self.home('fer2013', 'fer2013.csv'))):
             if ii == 0:
                 continue
-            label, pixels = line.split(',')
+            label, pixels, usage = line.split(',')
             assert int(label) < 7
-            assert pixels[-3] == '"'
-            assert pixels[0] == '"'
-            pixels = np.asarray(map(int, pixels[1:-3].split(' ')), dtype=np.uint8)
+            if 0:
+                assert pixels[-3] == '"'
+                assert pixels[0] == '"'
+                pixels = np.asarray(map(int, pixels[1:-3].split(' ')), dtype=np.uint8)
+            else:
+                pixels = np.asarray(map(int, pixels.split(' ')), dtype=np.uint8)
             meta.append({
-                'file': 'train.csv',
                 'label': int(label),
                 'pixels': pixels.reshape(48, 48),
-                })
-
-        # -- load test.csv
-        for ii, line in enumerate(open(self.home('test.csv'))):
-            if ii == 0:
-                continue
-            pixels, = line.split(',')
-            assert pixels[-3] == '"'
-            assert pixels[0] == '"'
-            pixels = np.asarray(map(int, pixels[1:-3].split(' ')), dtype=np.uint8)
-            meta.append({
-                'file': 'test.csv',
-                'label': 'unknown',
-                'pixels': pixels.reshape(48, 48),
+                'usage': usage.strip(' \n'),
                 })
 
         return meta
--- skdata/kaggle_facial_expression/main.py
+++ skdata/kaggle_facial_expression/main.py
@@ -1,18 +1,50 @@
 import sys
 import logging
+import time
 
 from skdata.kaggle_facial_expression.dataset \
-        import KaggleFacialExpression
+        import KaggleFacialExpression, FULL_URL
 
 usage = """
-Usage: main.py {fetch, show, clean_up}
+Usage: main.py {bib, clean_up, install, show, stats}
 """
 
-def main_fetch():
+def main_install():
     """
     Download the CIFAR10 data set to the skdata cache dir
     """
-    KaggleFacialExpression().fetch(True)
+    try:
+        KaggleFacialExpression().install(sys.argv[2])
+    except IndexError:
+        print "To download the Facial Recognition Challenge dataset"
+        print "log into Kaggle and retrieve", FULL_URL
+        print "then run this script again with the downloaded filename"
+        print "as an argument, like:"
+        print "python -m %s.main install fer2013.tgz" % __package__
+
+
+def main_bib():
+    """
+    Print out the proper citation for this dataset.
+    """
+    print open(KaggleFacialExpression().home('fer2013', 'fer2013.bib')).read()
+
+
+def main_stats():
+    """
+    Print some basic stats about the dataset (proving that it can be loaded).
+    """
+    self = KaggleFacialExpression()
+    t0 = time.time()
+    print 'loading dataset ...'
+    meta = self.meta
+    print ' ... done. (%.2f seconds)' % (time.time() - t0)
+    print ''
+    print 'n. Examples', len(meta)
+    #print 'Usages', set([m['usage'] for m in meta])
+    print 'n. Training', len([m for m in meta if m['usage'] == 'Training'])
+    print 'n. PublicTest', len([m for m in meta if m['usage'] == 'PublicTest'])
+    print 'n. PrivateTest', len([m for m in meta if m['usage'] == 'PrivateTest'])
 
 
 def main_show():
@@ -20,7 +52,6 @@ def main_show():
     Use glumpy to launch a data set viewer.
     """
     self = KaggleFacialExpression()
-    meta = self.meta
     from skdata.utils.glviewer import glumpy_viewer
     glumpy_viewer(
             img_array=[m['pixels'] for m in self.meta],
