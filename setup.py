0f51a129d93c7f54afe4b20faf277753be44a15b
Hannes Bretschneider <hannes@psi.utoronto.ca>
2013-12-10 20:33:37 -0500

    Fix setup.py to include data directories as package data

--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ if __name__ == "__main__":
           maintainer=MAINTAINER,
           packages=setuptools.find_packages(),
           include_package_data=True,
+          package_data={'skdata': ['data/*', 'descr/*']},
           maintainer_email=MAINTAINER_EMAIL,
           description=DESCRIPTION,
           license=LICENSE,
